6d4c40eb090b2176cbff470a90bf865cf4016a0a
Zsolti <nagy.zsolt.1989@gmail.com>
2016-06-16 23:36:37 +0200

    add carousel slideshow to portfolio items

--- scripts/script.js
+++ scripts/script.js
@@ -143,5 +143,15 @@ $(window).load(function() {
     });
 });
 
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("page-content").style.display = "block";
+}
 
 
