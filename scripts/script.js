8af6e245ee707fdcee3ff3c1a5cbc71024839b73
Zsolti <nagy.zsolt.1989@gmail.com>
2016-08-11 22:56:55 +0200

    major update and file addition

--- scripts/script.js
+++ scripts/script.js
@@ -1,157 +1,160 @@
 jQuery(document).ready(function()
 {
 
-    /*Interactivity to determine when an animated element in in view. In view elements trigger our animation*/
-    $(window).on("load",function()
+    /* Preloader */
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 5000);
+
+    //Animate progress-bars when shown
+    function showup()
     {
+        var animation_height = $(window).innerHeight() * 0.01;
 
-        //Animate progress-bars when shown
-        function showup()
+        //iterate through elements to see if its in view
+        $(".progress-bar").each(function()
         {
-            var animation_height = $(window).innerHeight() * 0.01;
+            var objectTop = $(this).offset().top;
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
 
-            //iterate through elements to see if its in view
-            $(".progress-bar").each(function()
+            if ( objectTop < windowBottom)
             {
-                var objectTop = $(this).offset().top;
-                var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-
-                if ( objectTop < windowBottom )
+                if (objectTop < windowBottom - animation_height)
                 {
-                    if (objectTop < windowBottom - animation_height)
-                    {
-                        each_bar_width = $(this).attr('aria-valuenow');
-                        $(this).width(each_bar_width + '%');
+                    each_bar_width = $(this).attr('aria-valuenow');
+                    $(this).width(each_bar_width + '%');
 
-                        $(document).ready(function () {
-                            $(window).stellar();
-
-                        });
-                    }
+                    $(document).ready(function () {
+                        $(window).stellar();
+                    });
                 }
-            });
-
-        }
-        showup();
-        $(window).scroll(function() {showup();});
-
-    });
-
-});
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
-});
-
-
-        $(document).ready(
-            function () {
-
-                $("html").niceScroll({
-                    cursorcolor: "rgba(30,30,30,.5)",
-                    zindex: 999,
-                    scrollspeed: 100,
-                    mousescrollstep: 50,
-                    cursorborder: "0px solid #fff"
-                });
-
-
             }
-        );
-
-        $(window).on("load", function () {
-            function bouncein() {
-                var animation_height = $(window).innerHeight() * 0.1;
-
-                $('.ninja').each(function () {
-
-                    var objectTop = $(this).offset().top;
-                    var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-
-                    if (objectTop < windowBottom) {
-                        if (objectTop < windowBottom - animation_height) {
-                            $(this).css({
-
-                                opacity: 1
-                            });
-                            $(this).addClass("bounceIn");
-
-                        }
-                    }
-                });
-            }
-
-            $('.ninja').css('opacity', 0);
-            bouncein();
-            $(window).scroll(function () {
-                bouncein();
-            });
         });
 
-        $(window).on("load", function () {
-            function fadein() {
-                var animation_height = $(window).innerHeight() * 0.1;
+    }
+    showup();
+    $(window).scroll(function() {showup();});
 
-                $('.icon').each(function () {
+    /* Show tooltips on skillbars */
 
-                    var objectTop = $(this).offset().top;
-                    var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-
-                    if (objectTop < windowBottom) {
-                        if (objectTop < windowBottom - animation_height) {
-                            $(this).css({
-
-                                opacity: 1
-                            });
-                            $(this).addClass("fadeIn");
-
-                        }
-                    }
-                });
-            }
-
-            $('.icon').css('opacity', 0);
-            fadein();
-            $(window).scroll(function () {
-                fadein();
-            });
-        });
-
-$(window).load(function() {
-    var $container = $('.animate-grid .gallary-thumbs');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
     });
-    $('.animate-grid .categories a').click(function() {
-        $('.animate-grid .categories .active').removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-filter');
+
+    /* Gallery item displays portfolio item detailed view*/
+
+    $('.gallary-item a').click(function(){
+        var page = $(this).attr('href');
+        $('#portfolio-item').empty();
+        $('html, body').animate({scrollTop:$('#portfolio').position().top}, "slow");
+        $('#portfolio-item').show().empty().load(page).slideDown("fast");
+        return false;
+    });
+
+
+    /* Skill Panel's animation */
+
+    function bouncein() {
+        var animation_height = $(window).innerHeight() * 0.1;
+
+        $('.ninja').each(function () {
+
+            var objectTop = $(this).offset().top;
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+            if (objectTop < windowBottom) {
+                if (objectTop < windowBottom - animation_height) {
+                    $(this).css({
+
+                        opacity: 1
+                    });
+                    $(this).addClass("bounceIn");
+
+                }
+            }
+        });
+    }
+
+    $('.ninja').css('opacity', 0);
+    bouncein();
+    $(window).scroll(function () {
+        bouncein();
+    });
+
+    /* Portfolio Grid */
+
+    $(window).load(function() {
+        var $container = $('.animate-grid .gallary-thumbs');
         $container.isotope({
-            filter: selector,
+            filter: '*',
             animationOptions: {
                 duration: 750,
                 easing: 'linear',
                 queue: false
             }
         });
-        return false;
+        $('.animate-grid .categories a').click(function() {
+            $('.animate-grid .categories .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
     });
+
+    /* Animate icons when in view in Contact panel*/
+
+    function fadein() {
+        var animation_height = $(window).innerHeight() * 0.1;
+
+        $('.icon').each(function () {
+
+            var objectTop = $(this).offset().top;
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight()
+
+            if (objectTop < windowBottom) {
+                if (objectTop < windowBottom - animation_height) {
+                    $(this).css({
+
+                        opacity: 1
+                    });
+                    $(this).addClass("fadeIn");
+
+                }
+            }
+        });
+    }
+
+    $('.icon').css('opacity', 0);
+    fadein();
+    $(window).scroll(function () {
+        fadein();
+    });
+
+    /* Scrolling animation for Parallax */
+
+    $("html").niceScroll({
+        cursorcolor:"rgba(30,30,30,.5)",
+        zindex:999,
+        scrollspeed:100,
+        mousescrollstep:50,
+        cursorborder:"0px solid #fff"
+    });
+
 });
 
-var myVar;
+/* Function for the Animated Close Button in Detailed portfolio view*/
 
-function myFunction() {
-    myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("page-content").style.display = "block";
+function toggler(divId) {
+    $("#" + divId).slideToggle("fast");
+    $("#" + divId).css("display", "block");
 }
 
 
